f7fff543316d3bfa23f5f8ebc090ca5fe2344468
huyhuynh408 <huyhuynh85@gmail.com>
2015-07-08 14:39:46 -0500

    testing red cross scraping

--- app/controllers/blood_banks_controller.rb
+++ app/controllers/blood_banks_controller.rb
@@ -1,2 +1,10 @@
+require 'open-uri'
+
 class BloodBanksController < ApplicationController
+
+  def self.uri
+    html_file = open('http://www.redcrossblood.org/give/drive/driveSearchList.jsp?zipSponsor=60613&range=10&zipFormat=true&sd=07/09/2015&ed=07/22/2015&dt=WB:DR:PL&&_requestid=371723')
+    puts html_file.read
+  end
+
 end
