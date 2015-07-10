37b8147e7419717a1f02af533e402bd19f521822
huyhuynh408 <huyhuynh85@gmail.com>
2015-07-09 19:45:43 -0500

    finding correct data to scrape

--- app/controllers/blood_banks_controller.rb
+++ app/controllers/blood_banks_controller.rb
@@ -5,6 +5,9 @@ class BloodBanksController < ApplicationController
   def self.uri
     html_file = open('https://donateblood.lifesource.org/SearchLocation.aspx?q=Chicago%2c+IL+60613%2c+USA&lat=41.9577761&lng=-87.6556468&d=5&start=07/08/2015&end=08/08/2015')
     puts html_file.read
+
+    doc = Nokogiri::HTML(File.open(html_file))
+    puts doc.search('.grid_12 > tr > td').map { |span| span.inner_text}
   end
 
 end
